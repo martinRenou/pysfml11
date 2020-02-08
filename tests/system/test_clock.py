e56c6b73f5d9e360b905ad99e6d7191e0189037e
martinRenou <martin.renou@gmail.com>
2020-02-08 12:58:01 +0100

    Add Clock tests
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- tests/system/test_clock.py
+++ tests/system/test_clock.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from pysfml.system import Clock, Time
 
 
@@ -5,3 +7,12 @@ def test_construction():
     clock = Clock()
 
     assert isinstance(clock.get_elapsed_time(), Time)
+    assert isinstance(clock.restart(), Time)
+
+
+def test_elapsed_time():
+    clock = Clock()
+
+    sleep(1)
+
+    assert clock.get_elapsed_time().as_seconds() >= 1
